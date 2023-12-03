e1a49e4681686abb075d7236d6f9ca306476d988
MegumiKasuga <1926195023@qq.com>
2023-12-03 15:57:20 +0800

    initialize

---
+++ src/main/java/org/fairingstudio/kuayue_website/KuayueWebsiteApplication.java
@@ -0,0 +1,13 @@
+package org.fairingstudio.kuayue_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuayueWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuayueWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fairingstudio/kuayue_website/KuayueWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fairingstudio.kuayue_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuayueWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
