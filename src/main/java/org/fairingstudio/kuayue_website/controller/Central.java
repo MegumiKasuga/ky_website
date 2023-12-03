865525eef84e1f26d384217c34ed33775cc62caa
MegumiKasuga <1926195023@qq.com>
2023-12-03 22:29:23 +0800

    结构化

--- src/main/java/org/fairingstudio/kuayue_website/controller/Central.java
+++ src/main/java/org/fairingstudio/kuayue_website/controller/Central.java
@@ -1,5 +1,6 @@
 package org.fairingstudio.kuayue_website.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class Central {
 
+    @Value("${server.port}")
+    private Integer port;
+
     @RequestMapping("/hello")
     @ResponseBody
     public String helloWorld(){
-        return "Hello World";
+        return "Hello World! Port is: " + port;
     }
 }
