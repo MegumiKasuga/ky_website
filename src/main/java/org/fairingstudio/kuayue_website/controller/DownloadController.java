175ac07821eec2de2446641906ddeeacf5389e2b
zlw380 <496607959@qq.com>
2023-12-06 08:42:29 +0800

    项目构建

--- src/main/java/org/fairingstudio/kuayue_website/controller/DownloadController.java
+++ src/main/java/org/fairingstudio/kuayue_website/controller/DownloadController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class UserController {
+public class DownloadController {
 
-    @RequestMapping("/login")
+    @RequestMapping("/download")
     public String download() {
-        return "login";
+        return "download";
     }
 }
