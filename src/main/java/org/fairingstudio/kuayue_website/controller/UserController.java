4a3314a3ceba9fdb209bcd4cf4861401b28c0752
zlw380 <496607959@qq.com>
2023-12-16 21:43:26 +0800

    修改登录请求

--- src/main/java/org/fairingstudio/kuayue_website/controller/UserController.java
+++ src/main/java/org/fairingstudio/kuayue_website/controller/UserController.java
@@ -1,14 +1,24 @@
 package org.fairingstudio.kuayue_website.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
 
     @RequestMapping("/login")
-    public String download() {
+    public String login() {
         return "login";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/userLogin")
+    public String userLogin(HttpSession session, Model model) {
+
+        System.out.println("执行了用户登录的控制器方法");
+        return "index";
     }
 }
