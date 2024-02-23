105a4895553fb7a42f44dca2cfeb45e23d246d15
zlw380 <496607959@qq.com>
2024-02-23 13:33:49 +0800

    删无用代码

--- src/test/java/org/fairingstudio/kuayue_website/KuayueWebsiteApplicationTests.java
+++ src/test/java/org/fairingstudio/kuayue_website/KuayueWebsiteApplicationTests.java
@@ -1,18 +1,12 @@
 package org.fairingstudio.kuayue_website;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.crypto.hash.Md5Hash;
-import org.apache.shiro.crypto.hash.SimpleHash;
-import org.apache.shiro.subject.Subject;
 import org.fairingstudio.kuayue_website.dao.ModFileDao;
 import org.fairingstudio.kuayue_website.dao.UserDao;
 import org.fairingstudio.kuayue_website.entity.*;
 import org.fairingstudio.kuayue_website.service.UserService;
 import org.fairingstudio.kuayue_website.util.IpUtils;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -31,33 +25,6 @@ class KuayueWebsiteApplicationTests {
         userDao.selectList(null).forEach(System.out::println);
 
         System.out.println("userService.getUserByName(\"admin\") = " + userService.getUserByName("admin"));
-    }
-
-    @Test
-    void testSecurityManager() {
-
-        //Subject subject = SecurityUtils.getSubject();
-        //System.out.println("subject = " + subject.toString());
-        //subject = org.apache.shiro.subject.support.DelegatingSubject@70ecf57b
-
-    }
-
-    @Test
-    void shiroMD5() {
-
-        String password = "123456";
-
-        Md5Hash md5Hash = new Md5Hash(password);
-        System.out.println("md5 加密："+md5Hash.toHex());
-        //带盐的 md5 加密，盐就是在密码明文后拼接新字符串，然后再进行加密
-        Md5Hash md5Hash2 = new Md5Hash(password,"kuayue");
-        System.out.println("md5 带盐加密："+md5Hash2.toHex());
-        //为了保证安全，避免被破解还可以多次迭代加密，保证数据安全
-        Md5Hash md5Hash3 = new Md5Hash(password,"kuayue",3);
-        System.out.println("md5 带盐三次加密："+md5Hash3.toHex());
-        //使用父类实现加密
-        SimpleHash simpleHash = new SimpleHash("MD5",password,"kuayue",3);
-        System.out.println("父类带盐三次加密："+simpleHash.toHex());
     }
 
     @Test
